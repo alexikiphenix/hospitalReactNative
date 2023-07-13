c72ef875b3a40fae89d6e24a9936ebdccb23b490
alexikiphenix <alexikiphenix@gmail.com>
2023-07-13 18:10:45 +0200

    adding scree messages

--- ecrans/tabs/index.js
+++ ecrans/tabs/index.js
@@ -1,4 +1,4 @@
-import {Text} from 'react-native';
+// import {Text} from 'react-native';
 import React from 'react';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import Home from '../Home';
