6de09c8bf43c50de4de8b223590b2ace15317b2f
alexikiphenix <alexikiphenix@gmail.com>
2023-07-15 23:47:42 +0200

    creating component ActivityItem v0.5

--- composantes/ActivityItem/index.js
+++ composantes/ActivityItem/index.js
@@ -1,17 +1,17 @@
-// import {Text, TouchableOpacity} from 'react-native';
-// import React from 'react';
-// import SVG_HOSPITAL from '../../assets/imgs/svg/undraw_medicine_b-1-ol.svg';
-// import dashboardStyles from './style';
-// import {FakeActivity} from '../../fakeData/fakeActivity';
+import {Text, TouchableOpacity} from 'react-native';
+import React from 'react';
+import SVG_HOSPITAL from '../../assets/imgs/svg/undraw_medicine_b-1-ol.svg';
+import dashboardStyles from './style';
+import {FakeActivity} from '../../fakeData/fakeActivity';
 
-// const ActivityItem = () => {
-//   return (
-//     <TouchableOpacity style={dashboardStyles.scrollableListItem}>
-//       <SVG_HOSPITAL width={60} height={60} />
-//       <Text style={dashboardStyles.mainText}>{item.mainText}</Text>
-//       <Text style={dashboardStyles.subText}>{item.subText}</Text>
-//     </TouchableOpacity>
-//   );
-// };
+const ActivityItem = () => {
+  return (
+    <TouchableOpacity style={dashboardStyles.scrollableListItem}>
+      <SVG_HOSPITAL width={60} height={60} />
+      <Text style={dashboardStyles.mainText}>{item.mainText}</Text>
+      <Text style={dashboardStyles.subText}>{item.subText}</Text>
+    </TouchableOpacity>
+  );
+};
 
-// export default ActivityItem;
+export default ActivityItem;
