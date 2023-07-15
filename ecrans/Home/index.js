a2258045e4c90845eefa38c31af4d50f6ea1778e
alexikiphenix <alexikiphenix@gmail.com>
2023-07-15 13:29:35 +0200

    adding style.js for managing  dashboardStyle

--- ecrans/Home/index.js
+++ ecrans/Home/index.js
@@ -1,11 +1,12 @@
 import {View, Text, ScrollView, Image} from 'react-native';
 import React from 'react';
+import dashboardStyles from './style';
 
 const Home = () => {
   return (
     <ScrollView>
       {/* Début du header */}
-      <View>
+      <View style={dashboardStyles.header}>
         <Text>John Doe</Text>
         <Image
           // eslint-disable-next-line react-native/no-inline-styles
